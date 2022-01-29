3a05fc6cc8ca3d43f904be3e367958ee2a597d37
FENG0409 <77607308+FENG0409@users.noreply.github.com>
2022-01-29 18:54:17 +0800

    add【filer】

---
+++ js/products.js
@@ -0,0 +1,106 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+
+let productModal = null;
+let delProductModal = null;
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'store_website',
+      products: [],
+      isNew: false,
+      tempProduct: {
+        imagesUrl: [],
+      },
+    }
+  },
+  mounted() {
+    productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+      keyboard: false
+    });
+
+    delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+      keyboard: false
+    });
+
+    // 取出 Token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkAdmin();
+  },
+  methods: {
+    // 登入及驗證
+    checkAdmin() {
+      const url = `${this.apiUrl}/api/user/check`;
+      axios.post(url)
+        .then(() => {
+          this.getData();
+        })
+        .catch((err) => {
+          alert(err.data.message)
+          window.location = 'index.html';
+        })
+    },
+    // 取得控制產品的API
+    getData() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products/all`;
+      axios.get(url).then((response) => {
+        this.products = response.data.products;
+      }).catch((err) => {
+        alert(err.data.message);
+      })
+    },
+    // 編輯產品API
+    updateProduct() {
+      let url = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+      let http = 'post';
+
+      if (!this.isNew) {
+        url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+        http = 'put'
+      }
+
+      axios[http](url, { data: this.tempProduct }).then((response) => {
+        alert(response.data.message);
+        productModal.hide();
+        this.getData();
+      }).catch((err) => {
+        alert(err.data.message);
+      })
+    },
+    openModal(isNew, item) {
+      if (isNew === 'new') {
+        this.tempProduct = {
+          imagesUrl: [],
+        };
+        this.isNew = true;
+        productModal.show();
+      } else if (isNew === 'edit') {
+        this.tempProduct = { ...item };
+        this.isNew = false;
+        productModal.show();
+      } else if (isNew === 'delete') {
+        this.tempProduct = { ...item };
+        delProductModal.show()
+      }
+    },
+    // 刪除產品API
+    delProduct() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+
+      axios.delete(url).then((response) => {
+        alert(response.data.message);
+        delProductModal.hide();
+        this.getData();
+      }).catch((err) => {
+        alert(err.data.message);
+      })
+    },
+    createImages() {
+      this.tempProduct.imagesUrl = [];
+      this.tempProduct.imagesUrl.push('');
+    },
+  },
+}).mount('#app');
